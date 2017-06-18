8fc8deabdf6c78a24bf535d88972f296e97ac8ba
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-06-18 22:52:37 +0100

    Making progress with the CSS

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ window.onload = function () {
 
   "use strict";
 
+  var screen = document.getElementById("screen");
+  var result = document.getElementById("result");
+  var history = document.getElementById("history");
+
+  if (result.scrollWidth > screen.clientWidth) {
+    result.innerText = "Digit Limit Met";
+  }
+  if (history.scrollWidth > screen.clientWidth) {
+    history.innerText = "Digit Limit Met";
+  }
+
 };
